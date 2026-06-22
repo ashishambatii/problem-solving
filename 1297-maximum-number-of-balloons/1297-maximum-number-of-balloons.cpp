b0c7f98e22e1dccae524d8cb4392d00694885c2c
Ashish Ambati <ashish.ambatii@gmail.com>
2026-06-22 17:19:49 +0530

    1297-MAXIMUM-NUMBER-OF-BALLOONS Stats: Time: 3 ms (12.33%), Space: 8.9 MB (84.03%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         map<char,int>map;
-        map['a']=0;
-        map['b']=0;
-        map['l']=0;
-        map['o']=0;
-        map['n']=0;
+      
         for(int i=0;i<text.size();i++){
             map[text[i]]++;   
         }
