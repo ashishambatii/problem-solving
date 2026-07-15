acffa2e447ecfd7e267029132910029fd4c18a3b
Ashish Ambati <ashish.ambatii@gmail.com>
2026-07-15 10:43:49 +0530

    3995-GCD-OF-ODD-AND-EVEN-SUMS Stats: Time: 0 ms (100%), Space: 8.4 MB (85.02%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int evencnt=0;
-        int oddcnt=0;
-        int i=1;
-        int sumodd=0;
-        int sumeven=0;
+        // int evencnt=0;
+        // int oddcnt=0;
+        // int i=1;
+        // int sumodd=0;
+        // int sumeven=0;
 
-        while(evencnt!=n && oddcnt!=n){
-            if(i%2==0 && evencnt<n){
-              sumeven+=i;
-              evencnt++;
-            }
-            else if(i%2!=0 && oddcnt<n){
-            sumodd+=i;
-            oddcnt++;
-            }
-        }
-        return gcd(sumeven,sumodd);
+        // while(evencnt!=n && oddcnt!=n){
+        //     if(i%2==0 && evencnt<n){
+        //       sumeven+=i;
+        //       evencnt++;
+        //     }
+        //     else if(i%2!=0 && oddcnt<n){
+        //     sumodd+=i;
+        //     oddcnt++;
+        //     }
+        // }
+        // return gcd(sumeven,sumodd);
+        return n;
     }
 };
