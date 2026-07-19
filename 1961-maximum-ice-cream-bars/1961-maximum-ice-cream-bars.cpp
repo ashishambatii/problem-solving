74a3f4f1482bc0817f1cc16dcbf234564beff2de
Ashish Ambati <ashish.ambatii@gmail.com>
2026-07-20 00:20:26 +0530

    1961-MAXIMUM-ICE-CREAM-BARS Stats: Time: 33 ms (47.26%), Space: 80.3 MB (71.35%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int n=costs.size();
         int cnt=0;
-        for(int i=0;i<n;i++){
-            if(coins>=costs[i]){
-                coins-=costs[i];
+        sort(costs.begin(),costs.end());
+        for(int i=0;i<costs.size();i++){
+            if(costs[i]<=coins && coins !=0){
                 cnt++;
+                coins-=costs[i];
             }
-            else{
-                break;
-            }
+
         }
         return cnt;
     }
