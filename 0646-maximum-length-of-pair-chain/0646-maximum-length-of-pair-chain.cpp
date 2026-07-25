2f355a06c92a4140786cd81c93e16754cc51536e
Ashish Ambati <ashish.ambatii@gmail.com>
2026-07-25 12:21:09 +0530

    0646-MAXIMUM-LENGTH-OF-PAIR-CHAIN Stats: Time: 47 ms (42.82%), Space: 27.3 MB (38.78%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int dp[2501][2501];
-    int n;
-   
-    int solve(int i, int p,vector<vector<int>>&nums){
-        if(i==n){
-            return 0;
-        }
-        if( dp[p+1][i]!=-1)
-           return dp[p+1][i];
-
-        int take=0;
-
-        if(p==-1 || nums[i][0]>nums[p][1]){
-            take=1+solve(i+1,i,nums);
-
-        }
-        int skip=solve(i+1,p,nums);
-        return dp[p + 1][i] = max(take, skip);
-    }
     int findLongestChain(vector<vector<int>>& pairs) {
-        n=pairs.size();
-        memset(dp, -1, sizeof(dp));
+        int n=pairs.size();
+        vector<int>dp(n,1);
         sort(pairs.begin(),pairs.end());
-        return solve(0,-1,pairs);
+        int maxlength=1;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(pairs[i][1]<pairs[j][0]){
+                    dp[j]=max(dp[j],dp[i]+1);
+                   
+                }
+            }
+             maxlength=max(maxlength,dp[i]);
+        }
+        return maxlength;
     }
 };
