b0692828e20d888d8fcf1063f437dad16883c5d9
Ashish Ambati <ashish.ambatii@gmail.com>
2026-08-10 09:41:55 +0530

    4363-MINIMUM-TOTAL-PRICE-AFTER-APPLYING-DISCOUNTS Stats: Time: 65 ms (100%), Space: 223.7 MB (100%) - LeetHub

--- 4363-minimum-total-price-after-applying-discounts/4363-minimum-total-price-after-applying-discounts.cpp
+++ 4363-minimum-total-price-after-applying-discounts/4363-minimum-total-price-after-applying-discounts.cpp
@@ -2,24 +2,38 @@ class Solution {
 public:
     double minPrice(vector<int>& prices, vector<int>& discounts) {
         double ans = 0;
-        priority_queue<int>p;
-         priority_queue<int>d;
-        for(int i=0;i<prices.size();i++){
-            p.push(prices[i]);
-        }
-        for(int i=0;i<discounts.size();i++){
-            d.push(discounts[i]);
-        }
-        while(!p.empty()){
-            if(!d.empty()){
-            double a = (100-d.top())/100.0;
-            ans+=(p.top()*a);
-                p.pop();
-                d.pop();
+        // priority_queue<int>p;
+        //  priority_queue<int>d;
+        // for(int i=0;i<prices.size();i++){
+        //     p.push(prices[i]);
+        // }
+        // for(int i=0;i<discounts.size();i++){
+        //     d.push(discounts[i]);
+        // }
+        // while(!p.empty()){
+        //     if(!d.empty()){
+        //     double a = (100-d.top())/100.0;
+        //     ans+=(p.top()*a);
+        //         p.pop();
+        //         d.pop();
+        //     }
+        //     else{
+        //         ans+=p.top();
+        //         p.pop();
+        //     }
+        // }
+        sort(prices.begin(),prices.end());
+        sort(discounts.begin(),discounts.end());
+        int j= discounts.size()-1;
+        for(int i=prices.size()-1;i>=0;i--){
+            if(j>=0){
+              double a =(100-discounts[j])/100.0;
+              ans+=prices[i]*a;
+              j--;
+
             }
             else{
-                ans+=p.top();
-                p.pop();
+                ans+=prices[i];
             }
         }
         return ans;
